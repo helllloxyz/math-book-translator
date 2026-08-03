9b134bd68975aa337e1c5a08fcaf3b863374a034
L.L <920817420@qq.com>
2026-08-03 18:29:50 +0800

    fix katex copy

--- frontend/src/utils/latexCopy.js
+++ frontend/src/utils/latexCopy.js
@@ -1,7 +1,25 @@
+const closestKatexElement = (node) => {
+  const element = node?.nodeType === Node.ELEMENT_NODE ? node : node?.parentElement
+  return element?.closest?.('.katex') || null
+}
+
+const rangeExpandedToKatexBoundaries = (range) => {
+  const copyRange = range.cloneRange()
+  const startKatex = closestKatexElement(range.startContainer)
+  const endKatex = closestKatexElement(range.endContainer)
+  const startTarget = startKatex?.closest('.katex-display') || startKatex
+  const endTarget = endKatex?.closest('.katex-display') || endKatex
+
+  if (startTarget) copyRange.setStartBefore(startTarget)
+  if (endTarget) copyRange.setEndAfter(endTarget)
+
+  return copyRange
+}
+
 export const selectionTextWithLatex = (selection) => {
   if (!selection || selection.rangeCount === 0) return ''
 
-  const range = selection.getRangeAt(0)
+  const range = rangeExpandedToKatexBoundaries(selection.getRangeAt(0))
   const div = document.createElement('div')
   div.appendChild(range.cloneContents())
   div.querySelectorAll('.katex').forEach(element => {
